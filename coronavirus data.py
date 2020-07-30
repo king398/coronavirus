f2decb5317bd48a94d478c8b520bc47c9ab16f21
king398 <mithilsalunkhe@outlook.com>
2020-07-30 17:48:49 +0530

    Update coronavirus data.py

--- coronavirus data.py
+++ coronavirus data.py
@@ -1,1 +1,6 @@
+from selenium import webdriver
 
+PATH = "C:\Program Files (x86)\chromedriver.exe"  # your path for the chrome web driver
+driver = webdriver.Chrome(PATH)  # for chrome
+
+driver.get()
