e0ec7b51427f87e8b01d197a43ccb779868348e4
king398 <mithilsalunkhe@outlook.com>
2020-07-31 11:32:13 +0530

    Update coronavirus data.py

--- coronavirus data.py
+++ coronavirus data.py
@@ -6,10 +6,9 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"  # your path for the chrome web
 driver = webdriver.Chrome(PATH)  # for chrome
 
 driver.get('https://www.bing.com/covid/local/india')
+
 search = driver.find_element_by_id('andhrapradesh_india')
-cases = driver.find_elements_by_class_name('confirmed')
-for case in cases:
-	print(case.text)
+
 search.click()
 
 
