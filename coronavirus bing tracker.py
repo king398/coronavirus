439d5e4f46c19431a3b2e424fa606f060b94a935
king398 <mithilsalunkhe@outlook.com>
2020-07-31 20:50:12 +0530

    Update coronavirus bing tracker.py
    added more examples

--- coronavirus bing tracker.py
+++ coronavirus bing tracker.py
@@ -10,10 +10,13 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"  # your path for the chrome web
 driver = webdriver.Chrome(PATH)  # for chrome
 
 driver.get('https://www.bing.com/covid/local/india')
-time.sleep(10)
+time.sleep(5)
 state_name = driver.find_element_by_id("maharashtra_india")  # enter your state id on bing covid-19 tracker
 state_name.click()
-district_id = ['nanded_maharashtra_india']  # districts you want to see
+district_id = ['nanded_maharashtra_india', 'mumbai_maharashtra_india', 'pune_maharashtra_india',
+               'thane_maharashtra_india', 'raigarh_maharashtra_india',
+               'nashik_maharashtra_india', 'palghar_maharashtra_india',
+               'nashik_maharashtra_india']  # districts you want to see
 total_case = []
 ie = 0
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -22,17 +25,14 @@ for i in district_id:
 	name_of_state = driver.find_element_by_id(i)
 	name_of_state.click()
 	cases = driver.find_elements_by_class_name('confirmed')
-
+	
 	for case in cases:
 		case_int = locale.atoi(case.text)
 
 		if case_int <= 17297296:  # change the value to compare with with the latest covid count
 			print('{}:{}'.format(i, case.text))
-	print(ie)
+
 	ie += 1
-	print(ie)
 
-print(ie)
-
-time.sleep(10)
+time.sleep(5)
 driver.quit()
