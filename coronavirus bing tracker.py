18e2d5dfe05830aac8296781dae763a3da445c96
king398 <mithilsalunkhe@outlook.com>
2020-07-31 22:40:36 +0530

    Update coronavirus bing tracker.py
    new name added

--- coronavirus bing tracker.py
+++ coronavirus bing tracker.py
@@ -16,7 +16,23 @@ state_name.click()
 district_id = ['nanded_maharashtra_india', 'mumbai_maharashtra_india', 'pune_maharashtra_india',
                'thane_maharashtra_india', 'raigarh_maharashtra_india',
                'nashik_maharashtra_india', 'palghar_maharashtra_india',
-               'nashik_maharashtra_india']  # districts you want to see
+               'nashik_maharashtra_india', 'aurangabad_maharashtra_india', 'solapur_maharashtra_india',
+               'kolhapur_maharashtra_india', 'nagpur_maharashtra_india',
+               'ahmednagar_maharashtra_india', 'satara_maharashtra_india',
+               'dhule_maharashtra_india', 'akola_maharashtra_india',
+               'sangli_maharashtra_india',
+               'amravati_maharashtra_india',
+               'latur_maharashtra_india',
+               'jalna_maharashtra_india',
+               'ratnagiri_maharashtra_india',
+               'buldana_maharashtra_india',
+               'osmanabad_maharashtra_india', 'yavatmal_maharashtra_india',
+               'beed_maharashtra_india', 'parbhani_maharashtra_india',
+               'nandurbar_maharashtra_india', 'washim_maharashtra_india',
+               'hingoli_maharashtra_india', 'chandrapur_maharashtra_india',
+               'sindhudurg_maharashtra_india', 'gondia_maharashtra_india',
+               'gadchiroli_maharashtra_india', 'bhandara_maharashtra_india',
+               'wardha_maharashtra_india']  # districts you want to see
 total_case = []
 ie = 0
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -25,7 +41,7 @@ for i in district_id:
 	name_of_state = driver.find_element_by_id(i)
 	name_of_state.click()
 	cases = driver.find_elements_by_class_name('confirmed')
-	
+
 	for case in cases:
 		case_int = locale.atoi(case.text)
 
