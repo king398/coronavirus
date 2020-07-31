cd8417f4bb6ff7e74c893401c27082f45cfccb9d
king398 <mithilsalunkhe@outlook.com>
2020-07-31 11:48:44 +0530

    Update coronavirus data.py

--- coronavirus data.py
+++ coronavirus data.py
@@ -8,14 +8,14 @@ driver = webdriver.Chrome(PATH)  # for chrome
 driver.get('https://www.bing.com/covid/local/india')
 search = driver.find_element_by_id('maharashtra_india')  # enter your state id on bing covid-19 tracker
 search.click()
-states_id = ['ahmednagar_maharashtra_india']  # districts you want to see
+states_id = ['ahmednagar_maharashtra_india', 'nanded_maharashtra_india']  # districts you want to see
 total_case = []
 for i in states_id:
 	name_of_state = driver.find_element_by_id(i)
 	name_of_state.click()
 	cases = driver.find_elements_by_class_name('confirmed')
 	for case in cases:
-		print(case.text)
+		print('{}:{}'.format(i, case.text))
 
 time.sleep(10)
 driver.quit()
