56763446783aff88c309c64dd5a266535f969126
king398 <mithilsalunkhe@outlook.com>
2020-08-01 11:05:34 +0530

    finally over now change what you want

--- data/__init__.py
+++ data/__init__.py
@@ -2,11 +2,14 @@ def coronavirus_case(Path, state_name_element, district_id):
 	from selenium import webdriver
 	import time
 	import locale
+	import xlwt
+	from xlwt import Workbook
 	total_case = {}
 	case_1 = []
 	case_2 = []
 	driver = webdriver.Chrome(Path)
 	driver.get('https://www.bing.com/covid/local/india')
+	time.sleep(5)
 	state_name = driver.find_element_by_id(state_name_element)
 	state_name.click()
 
@@ -23,9 +26,22 @@ def coronavirus_case(Path, state_name_element, district_id):
 				print('{}:{}'.format(i, case.text))
 				total_case.update({i: case.text})
 	case_1 = total_case.keys()
-	case_2 = [total_case.values()]
-	print(case_1)
-	print(case_2)
-	print(total_case)
+	case_2 = total_case.values()
+
+	excelfile = Workbook()
+	sheet1 = excelfile.add_sheet("sheet1")
+	i = 0
+	ie = 0
+	for writingnames in case_1:
+		sheet1.write(i, 0, writingnames)
+
+		i += 1
+	for writing in case_2:
+		sheet1.write(ie, 1, writing)
+		ie += 1
+	filename = time.time()
+
+	excelfile.save("sample{}.xls".format(filename))  # enter the name of the file in the quotation marks
+
 	time.sleep(5)
 	driver.quit()
