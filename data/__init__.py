8b56f36d716e5453e0d497b17d30d20100e2b463
king398 <mithilsalunkhe@outlook.com>
2020-08-01 10:30:10 +0530

    ]
    ]

--- data/__init__.py
+++ data/__init__.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-import time
-import locale
-
-total_case = {}
-
-
 def coronavirus_case(Path, state_name_element, district_id):
+	from selenium import webdriver
+	import time
+	import locale
+	total_case = {}
 	driver = webdriver.Chrome(Path)
 	driver.get('https://www.bing.com/covid/local/india')
 	state_name = driver.find_element_by_id(state_name_element)
@@ -23,8 +20,6 @@ def coronavirus_case(Path, state_name_element, district_id):
 			if case_int <= 17297296:  # change the value to compare with with the latest covid count
 				print('{}:{}'.format(i, case.text))
 				total_case.update({i: case.text})
-		time.sleep(5)
+	print(total_case)
+	time.sleep(5)
 	driver.quit()
-
-
-print(total_case)
