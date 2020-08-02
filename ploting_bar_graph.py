6ace4a5b0bf74f761bc8a79300dbb92aa3daf303
king398 <mithilsalunkhe@outlook.com>
2020-08-02 18:27:01 +0530

    ]

--- ploting_bar_graph.py
+++ ploting_bar_graph.py
@@ -2,4 +2,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 execel_file = pd.read_excel(r'coronavirus\sample-maharashtra.xls')
-execel_file.to_csv(r'coronavirus\sample-maharashtra.csv')
+execel_file.to_csv(r'coronavirus\sample-maharashtra.csv', index=None, header=True)
